99fdcd9a656cfc2ea0055d2f13ba286671c5be00
Rajeet013 <rajeet013@gmail.com>
2026-06-05 14:07:27 +0600

    School part needs to be done

---
+++ app/components/schoolLife/Event.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Event = () => {
+  return (
+    <div className='flex flex-col md:flex-row'>
+        <div className='bg-white w-1/2'>
+            <p className='text-black'>Event and Traditions</p>
+        </div>
+        <div className='bg-[#F5F5F5] w-1/2'>
+            <p className='text-black'>Transportations</p>    
+        </div>
+    </div>
+  )
+}
+
+export default Event
--- app/schoolLife/page.tsx
+++ app/schoolLife/page.tsx
@@ -1,9 +1,11 @@
+import Event from "../components/schoolLife/Event";
 import HeroSchool from "../components/schoolLife/Hero_school";
 
 const page = () => {
   return (
     <div>
       <HeroSchool />
+      <Event />
     </div>
   );
 };
